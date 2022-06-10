97039e105f4d94eb53e40929427b664eb09a39af
Kunj <shahkunj42@gmail.com>
2022-06-10 00:09:31 -0400

    commit tree 79808f99a6621e91790303c2067ace76e9d64939

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(array, name){
+    let newArray = array.filter(function(string){
+        return string.toUpperCase() == name.toUpperCase();
+    });
+    return newArray
+};
+
+// function findMatching(drivers, matchedDriver) {
+//     return (drivers.filter(match => {
+//        return match.toLowerCase() === matchedDriver.toLowerCase()
+//     }))
+// }
+
+let fuzzyMatch = (array, name) => {
+    let newArray = array.filter(function(string){
+        return string.slice(0,2) == name;
+    });
+    return newArray
+}
+const matchName = function(array, string){
+    let sameName = array.filter(function(person) {
+         return person.name == string
+    })
+    return sameName
+}
